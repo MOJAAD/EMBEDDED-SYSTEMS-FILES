3370d727762b5a2432239045785ddfff5851675a
MOJAAD <mojaad@outlook.com>
2020-07-24 17:50:03 +0430

    completed

---
+++ lesson1/main.c
@@ -0,0 +1,18 @@
+
+int main()
+{
+  int counter=0;
+  ++counter;
+  ++counter;
+  ++counter;
+  ++counter;
+  ++counter;
+  ++counter;
+  ++counter;
+  ++counter;
+  ++counter;
+  ++counter;
+  ++counter;
+  
+  return 0;
+}
