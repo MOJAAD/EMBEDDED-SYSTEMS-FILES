cafd3bdc6a1113898c01cfcfa9a33882a8e34e19
MOJAAD <mojaad@outlook.com>
2020-07-24 18:16:13 +0430

    completed 3

--- lesson3/main.c
+++ lesson3/main.c
@@ -1,15 +1,13 @@
-
+int counter=0;
 int main()
 {
-  int counter=0;
-  while(counter <21){
-    ++counter;
-    if ((counter & 1) != 0){
-      //odd
-    }
-    else {
-    //even
-    }
+  int *p_int;
+  p_int = &counter;
+  while(*p_int <21){
+    ++(*p_int);
   }
+  //danguares!
+  p_int = (int *)0x20000002u;
+  *p_int = 0xDEADBEEF;
   return 0;
 }
