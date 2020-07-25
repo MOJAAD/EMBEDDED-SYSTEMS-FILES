01e9f1c74da5c993f4b9bde818563ad1f02d3896
MOJAAD <mojaad@outlook.com>
2020-07-25 17:03:56 +0430

    lesson5 completed

--- lesson5/main.c
+++ lesson5/main.c
@@ -1,8 +1,10 @@
+//include "lm4f120h5qr.h"     (i don't have board!)
+
 #define RCGCGPIO (*((unsigned int *)0x400FE608u))
 #define GPIO_BASE (*((unsigned int *)0x40025000u))
-#define GPIO_DIR (*((unsigned int *)(GPIO_BASE + 0x400u)))
-#define GPIO_DEN (*((unsigned int *)(GPIO_BASE + 0x51Cu)))
-#define GPIO_DATA (*((unsigned int *)(GPIO_BASE + 0x3FCu)))
+#define GPIO_DIR (*((unsigned int *)(GPIO_BASE + 0x00000400u)))
+#define GPIO_DEN (*((unsigned int *)(GPIO_BASE + 0x0000051Cu)))
+#define GPIO_DATA (*((unsigned int *)(GPIO_BASE + 0x000003FCu)))
 
 
 int main()
