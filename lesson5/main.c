edf47afe7d92f07699137acc1642c4b14e702659
MOJAAD <mojaad@outlook.com>
2020-07-25 16:23:47 +0430

    lesson5 added

---
+++ lesson5/main.c
@@ -0,0 +1,26 @@
+#define RCGCGPIO (*((unsigned int *)0x400FE608u))
+#define GPIO_BASE (*((unsigned int *)0x40025000u))
+#define GPIO_DIR (*((unsigned int *)(GPIO_BASE + 0x400u)))
+#define GPIO_DEN (*((unsigned int *)(GPIO_BASE + 0x51Cu)))
+#define GPIO_DATA (*((unsigned int *)(GPIO_BASE + 0x3FCu)))
+
+
+int main()
+{
+  RCGCGPIO = 0x20u;
+  GPIO_DIR = 0x0Eu;
+  GPIO_DEN = 0x0Eu;
+  while(1){
+    GPIO_DATA = 0x02u;      //blink on
+    int counter=0;
+    while(counter<1000000){ //delay loop
+      ++counter;
+    }
+    GPIO_DATA = 0x00u;      //blink off
+    counter=0;
+    while(counter<1000000){ //delay loop
+        ++counter;
+    }
+  }
+  return 0;
+}
